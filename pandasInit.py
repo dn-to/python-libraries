344e243bda645fcb92aab0ca33c7aacf9303189a
danito <danimtorresh@gmail.com>
2025-08-26 15:06:20 -0600

    Finishing first steps with pandas

--- pandasInit.py
+++ pandasInit.py
@@ -5,18 +5,26 @@ dataFrame = pandas.read_csv("./file.txt", sep='\t')
 #print(dataFrame)
 
 #------>Retrieving only 5 first lines
-#print(dataFrame.head(5))
+#ndataFrame= dataFrame.head(5)
+#print(ndataFrame.sort_values("Edad", ascending=False)) #sort
 
 #------>Retrieving specific columns
-#print(dataFrame.columns)
+#print(dataFrame.columns) #name of columns
 #print(dataFrame[["Objeto" , "Nombre"]])
+#print(dataFrame["Edad"].mean()) #average of "Edad"
+dataFrame["Mayor_de_30"] = dataFrame["Edad"] > 30 #create new column
+#print(dataFrame)
 
 #------>Retrieving rows with specific conditions
-print(dataFrame[dataFrame["Edad"]<20])
+#print(dataFrame[dataFrame["Edad"]<20])
+#gbDF=(dataFrame[(dataFrame["Gustos"]=="Música")  & (dataFrame["Edad"]<20)])
+#gbDF=(dataFrame[(dataFrame["Gustos"]=="Música")  | (dataFrame["Edad"]<20)])
+#print(gbDF)
 
 #------>Retrieving rows ordering by count
-gbDF=dataFrame.groupby("Gustos")["Nombre"].count()
-#print(gbDF)
-#print(gbDF.sort_values(ascending=False))
+#gbDF=dataFrame.groupby("Gustos")["Nombre"].count()
+#print((dataFrame.groupby("Gustos")["Edad"].mean()).sort_values(ascending=False))
 
-#------>Retrieving rows with certain Gusto and Edad>20
+
+
+dataFrame.to_csv("resultado.csv", index=False)

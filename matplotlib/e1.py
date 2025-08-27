f0ef9c71b778400a631f6ab3c35c740388b93791
danito <danimtorresh@gmail.com>
2025-08-26 19:30:15 -0600

    Finishing today

--- matplotlib/e1.py
+++ matplotlib/e1.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 x=[1,2,3,4,5]
 y=[2,4,6,8,10]
-plt.plot(x, y)
-plt.title("Ejercicio 1: Línea básica")
+plt.plot(x, y, color="#F8226D", linestyle="--", linewidth=3)
+#plt.title("Ejercicio 1: Línea básica")
+plt.title("Ejercicio 1: Mi primera gráfica")
 plt.xlabel("Eje x")
 plt.ylabel("Eje y")
 plt.show()
+
